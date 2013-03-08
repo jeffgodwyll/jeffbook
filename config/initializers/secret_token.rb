c6984967edcbb789a0e2e83cef153d6b0e24ded5
Jeff <rey12rey@gmail.com>
2013-03-08 12:13:36 +0000

    I created a copycat lolbook app and added statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeffbook::Application.config.secret_token = '679f8074ce9aba4c18e5b679e652dd0f29780737791ac205c93eaafd647ec7fee32a445281781da7fb54995c4b2c69f05e6982807998a5d510bf51203484c378'
